e0111325795811f3eeac0b0515c37b1adcad6119
Ethan Lee <elee@xpanxion.com>
2023-05-16 10:52:22 -0400

    Finish up to props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
-
+import { Message } from "./Message";
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <Message text="hello world" />
+    </div>
+  );
 }
 
 export default App;
---
+++ my-app/src/Message.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Message = ({text}) => {
+  return <div>{text}</div>;
+};
